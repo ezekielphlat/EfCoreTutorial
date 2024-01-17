12dcddaec93eed672f63061c4b53f9d2b482e510
Ezekiel Abodesegun <eabodesegun@appzonegroup.com>
2024-01-17 06:05:39 +0100

    insert data

--- EfCoreBasics/Program.cs
+++ EfCoreBasics/Program.cs
@@ -8,17 +8,30 @@ namespace EfCoreBasics
         static void Main(string[] args)
         {
            
-
+            // insert manager into the db because we need the ID inside employee
             using(var context = new AppDbContext())
             {
-                var newManager = new Manager();
-                newManager.MngLastName = "Bob";
-                newManager.MngFirstName = "Willer";
-                newManager.MngSalary = 1000;
+                //var newManager = new Manager();
+                //newManager.MngLastName = "Bob";
+                //newManager.MngFirstName = "Willer";
+                //newManager.MngSalary = 1000;
 
-                context.Managers.Add(newManager);
+                //context.Managers.Add(newManager);
+                //context.SaveChanges();
+                Console.WriteLine("manager inserted");
+
+
+                var nEmployee = new Employee();
+                nEmployee.EmpFirstName = "Abisola";
+                nEmployee.EmpLastName = "Adebambo";
+                nEmployee.EmpSalary = 1000;
+                nEmployee.ManagerId = 2;
+
+                context.Employees.Add(nEmployee);
                 context.SaveChanges();
+                Console.WriteLine("employee inserted");
             }
+
 
             Console.ReadLine();
         }
